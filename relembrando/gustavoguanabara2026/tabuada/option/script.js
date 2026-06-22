fc389dc8e024c02ee489f778d08b263c572cd13b
Pedro Muniz <joao.munnyz@gmail.com>
2026-06-22 18:13:11 -0300

    tabuada

---
+++ relembrando/gustavoguanabara2026/tabuada/option/script.js
@@ -0,0 +1,20 @@
+let btn = document.getElementById('btn')
+btn.addEventListener('click', click)
+function click(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('tab')
+        if (num.value.length == 0) {
+            window.alert('erro, insira um numero')
+        } else {
+            let n = Number(num.value);
+            let c = 1
+            tab.innerHTML = ' '
+            console.log(c);
+                while ( c <= 10 ) {
+                let item = document.createElement('option')
+                item.text = `${n}x${c}=${n*c}`;
+                c++ ;
+                tab.appendChild(item);
+        }
+        } 
+}
