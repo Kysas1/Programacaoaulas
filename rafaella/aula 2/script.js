00c0326e0044c69edc8a3964272b24f7b2be9c50
Pedro Muniz <joao.munnyz@gmail.com>
2026-06-23 18:46:03 -0300

    Projeto-Tarefa

--- rafaella/aula 2/script.js
+++ rafaella/aula 2/script.js
@@ -1,19 +1,24 @@
 function adicionarTarefa() {
     
     let input = document.getElementById('input');
-    let inputT = input.value;
-    if (input.value.length == ' ') {
-        alert('[Erro] Adcione uma tarefa !')
+    let inputT = input.value.trim();
+    let p = document.getElementById('mensagem')
+    if (inputT == '') {
+        alert('[Erro] Adcione uma tarefa !') ;
+        let mensagemerro = 'Adcione uma Tarefa no campo !';
+        p.textContent = mensagemerro ;
+        p.style.color ='#A34743'
     } else {
         console.log(inputT);
-    let mensagem = 'Tarefa adcionada com sucesso!';
-    document.getElementById("mensagem").textContent = mensagem;
-    let listatarefas = document.getElementById('listatarefas');
-    let novatarefa = document.createElement('li');
-    listatarefas.appendChild(novatarefa);
-    novatarefa.textContent = inputT ; 
-    input.value = '' ; 
+        let mensagem = 'Tarefa adcionada com sucesso!';
+        p.textContent = mensagem;
+        p.style.color = '#28A745'
+        let listatarefas = document.getElementById('listatarefas');
+        let novatarefa = document.createElement('li');
+        listatarefas.appendChild(novatarefa);
+        novatarefa.textContent = inputT ; 
+       
     }
-    
+    input.value = '' ; 
 
 }
