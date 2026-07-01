6175865cbac9b9ebfb6ec57012b21666ff712215
Pedro Muniz <joao.munnyz@gmail.com>
2026-07-01 16:24:18 -0300

    att

--- rafaella/aula 2/script.js
+++ rafaella/aula 2/script.js
@@ -1,5 +1,5 @@
-function adicionarTarefa() {
-    
+let tarefas = []
+function adicionarTarefa() {  
     let input = document.getElementById('input');
     let inputT = input.value.trim();
     let p = document.getElementById('mensagem')
@@ -13,12 +13,51 @@ function adicionarTarefa() {
         let mensagem = 'Tarefa adcionada com sucesso!';
         p.textContent = mensagem;
         p.style.color = '#28A745'
-        let listatarefas = document.getElementById('listatarefas');
-        let novatarefa = document.createElement('li');
-        listatarefas.appendChild(novatarefa);
-        novatarefa.textContent = inputT ; 
-       
+        tarefas.push(inputT);
+        renderizar();
     }
     input.value = '' ; 
+}
 
+function renderizar(){
+    let listatarefas = document.getElementById('listatarefas');
+    listatarefas.innerHTML = ''
+    for(let c = 0 ;c < tarefas.length ; c++) {
+        let novatarefa = document.createElement('li');
+        novatarefa.textContent = tarefas[c] ; 
+
+        let botaoRemover = document.createElement('button');
+        botaoRemover.className ='remover';
+        botaoRemover.textContent ='Remover'
+        botaoRemover.onclick = () => Removertarefa(c);
+
+        let botaoEditar = document.createElement('button');
+        botaoEditar.className ='editar';
+        botaoEditar.textContent ='Editar'
+        botaoEditar.onclick = () => editarTarefa(c);
+        novatarefa.appendChild(botaoEditar);
+        novatarefa.appendChild(botaoRemover);
+        listatarefas.appendChild(novatarefa);   
+    }    
+}
+
+function Removertarefa(c){
+    tarefas.splice(c, 1);
+    renderizar()
+}
+
+function editarTarefa(c) {
+    let tarefaEditada = prompt('Edite sua tarefa: ');
+    if (tarefaEditada.trim() !== ''){
+        tarefas[c] = tarefaEditada;
+        renderizar();
+    }
+}
+
+function limparLista(c){
+    tarefas.length = 0
+    renderizar();
+    let p = document.getElementById('mensagem')
+    p.textContent = 'Tarefas Removidas';
+    p.style.color = '#7e0404'
 }
